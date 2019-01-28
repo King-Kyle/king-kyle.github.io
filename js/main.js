2d08aade67b53a12e165dea902a54fbe57ece5df
Kyle <kylek918@icloud.com>
2019-01-28 23:13:49 +0200

    customization

--- js/main.js
+++ js/main.js
@@ -1,62 +1,78 @@
-
-/* ===================================================================
- * # MENU SCROLL SPY
+ /* ===================================================================
+ * # MENU UI
  *
  * ------------------------------------------------------------------- */
 
+ // SHOW/HIDE NAV
 
- (function() {
-   'use strict';
+ // Hide Header on on scroll down
+ var didScroll;
+ var lastScrollTop = 0;
+ var delta = 5;
+ var navbarHeight = $('header').outerHeight();
 
-   var section = document.querySelectorAll(".section");
-   var sections = {};
-   var i = 0;
-
-   Array.prototype.forEach.call(section, function(e) {
-     sections[e.id] = e.offsetTop;
-   });
-
-   window.onscroll = function() {
-     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-
-     for (i in sections) {
-       if (sections[i] <= scrollPosition) {
-         document.querySelector('.active').setAttribute('class', ' ');
-         document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
-       }
-     }
-   };
- })();
-
-
-/* ===================================================================
- * # SMOOTH SCROLL
- *
- * ------------------------------------------------------------------- */
-
-
- jQuery(document).ready(function($) {
-
-    $('.smoothscroll').on('click',function (e) {
- 	    e.preventDefault();
-
- 	    var target = this.hash,
- 	    $target = $(target);
-
- 	    $('html, body').stop().animate({
- 	        'scrollTop': $target.offset().top
- 	    }, 800, 'swing', function () {
- 	        window.location.hash = target;
- 	    });
- 	});
-
+ $(window).scroll(function(event){
+     didScroll = true;
  });
+
+ setInterval(function() {
+     if (didScroll) {
+         hasScrolled();
+         didScroll = false;
+     }
+ }, 250);
+
+ function hasScrolled() {
+     var st = $(this).scrollTop();
+
+     // Make sure they scroll more than delta
+     if(Math.abs(lastScrollTop - st) <= delta)
+         return;
+
+     // If they scrolled down and are past the navbar, add class .nav-up.
+     // This is necessary so you never see what is "behind" the navbar.
+     if (st > lastScrollTop && st > navbarHeight){
+         // Scroll Down
+         $('header').removeClass('show-nav').addClass('hide-nav');
+         $('.nav-toggle').removeClass('open');
+         $('.menu-left').removeClass('collapse');
+     } else {
+         // Scroll Up
+         if(st + $(window).height() < $(document).height()) {
+             $('header').removeClass('hide-nav').addClass('show-nav');
+         }
+     }
+
+     lastScrollTop = st;
+ }
+
+ /* ===================================================================
+  * # SMOOTH SCROLL
+  *
+  * ------------------------------------------------------------------- */
+
+
+  jQuery(document).ready(function($) {
+
+     $('.smoothscroll').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash,
+  	    $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 800, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
+
+  });
 
  /* ===================================================================
   * # HIDE SCROLL AT FOOTER
   *
   * ------------------------------------------------------------------- */
-
 
   jQuery(document).ready(function(){
     jQuery(window).scroll(function() {
