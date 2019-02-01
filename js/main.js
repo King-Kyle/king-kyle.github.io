a6797bd38712b2399cfd14c3d52a656e7bb22e67
Kyle <kylek918@icloud.com>
2019-02-01 02:38:10 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,84 +1,18 @@
- /* ===================================================================
- * # MENU UI
- *
- * ------------------------------------------------------------------- */
-
- // SHOW/HIDE NAV
-
- // Hide Header on scroll down
- var didScroll;
- var lastScrollTop = 0;
- var delta = 5;
- var navbarHeight = $('header').outerHeight();
-
- $(window).scroll(function(event){
-     didScroll = true;
- });
-
- setInterval(function() {
-     if (didScroll) {
-         hasScrolled();
-         didScroll = false;
-     }
- }, 250);
-
- function hasScrolled() {
-     var st = $(this).scrollTop();
-
-     // Make sure they scroll more than delta
-     if(Math.abs(lastScrollTop - st) <= delta)
-         return;
-
-     // If they scrolled down and are past the navbar, add class .nav-up.
-     // This is necessary so you never see what is "behind" the navbar.
-     if (st > lastScrollTop && st > navbarHeight){
-         // Scroll Down
-         $('header').removeClass('show-nav').addClass('hide-nav');
-         $('.nav-toggle').removeClass('open');
-         $('.menu-left').removeClass('collapse');
-     } else {
-         // Scroll Up
-         if(st + $(window).height() < $(document).height()) {
-             $('header').removeClass('hide-nav').addClass('show-nav');
-         }
-     }
-
-     lastScrollTop = st;
- }
-
- /* ===================================================================
-  * # SMOOTH SCROLL
-  *
-  * ------------------------------------------------------------------- */
-
-  jQuery(document).ready(function($) {
-
-     $('.smoothscroll').on('click',function (e) {
-  	    e.preventDefault();
-
-  	    var target = this.hash,
-  	    $target = $(target);
-
-  	    $('html, body').stop().animate({
-  	        'scrollTop': $target.offset().top
-  	    }, 800, 'swing', function () {
-  	        window.location.hash = target;
-  	    });
-  	});
-
-  });
-
- /* ===================================================================
-  * # HIDE SCROLL AT FOOTER
-  *
-  * ------------------------------------------------------------------- */
-
-  jQuery(document).ready(function(){
-    jQuery(window).scroll(function() {
-      if (jQuery('body').height() <= (jQuery(window).height() + jQuery(window).scrollTop())) {
-        jQuery('.home-scroll').fadeOut("slow");
-      } else {
-        jQuery(".home-scroll").fadeIn("slow");
-      }
-    });
-  });
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
